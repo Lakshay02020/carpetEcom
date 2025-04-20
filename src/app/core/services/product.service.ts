84fcd8bf52a6466108b454b0c967db8a1ce4b735
Lakshay Singla <lakshay02singla@gmail.com>
2025-04-20 11:12:19 +0530

    Get Api done

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product.model'; // Adjust the import path as necessary
+import { environment } from '../../../environments/environment'; // Adjust the import path as necessary
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://localhost:8090/api/products';
+  private apiUrl = `${environment.productApi}/products`;
 
   constructor(private http: HttpClient) {}
 
