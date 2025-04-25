44e91496cc4ca2c84528da7fb3f9be3e725af252
Lakshay Singla <lakshay02singla@gmail.com>
2025-04-25 14:34:57 +0530

    Nav Bar fixed + Cart Intial Desing Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule}  from '@angular/router';
+import { NavBarPage } from "./features/shared/pages/nav-bar/nav-bar.page";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],  // Use RouterOutlet to load routed components
+  imports: [RouterOutlet, RouterModule, NavBarPage],  // Use RouterOutlet to load routed components
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  // Corrected to styleUrls (note the plural form)
 })
