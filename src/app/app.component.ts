a0a488e16ac216793a30ef76cd2efadef10fe699
Lakshay Singla <lakshay02singla@gmail.com>
2025-06-21 14:09:45 +0530

    Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule}  from '@angular/router';
 import { NavBarPage } from "./features/shared/pages/nav-bar/nav-bar.page";
+import { FooterComponent } from './features/shared/pages/footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, NavBarPage],  // Use RouterOutlet to load routed components
+  imports: [RouterOutlet, RouterModule, NavBarPage, FooterComponent],  // Use RouterOutlet to load routed components
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  // Corrected to styleUrls (note the plural form)
 })
