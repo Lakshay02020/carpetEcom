92fa945b6ee535ab2d866c3966dcd389300bc023
Lakshay Singla <lakshay02singla@gmail.com>
2025-06-21 17:22:15 +0530

    Fixed hamburger menu:

--- src/app/features/shared/pages/nav-bar/nav-bar.page.ts
+++ src/app/features/shared/pages/nav-bar/nav-bar.page.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule}  from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterModule],  // Use RouterOutlet to load routed components
+  imports: [RouterModule, CommonModule],  // Use RouterOutlet to load routed components
   templateUrl: './nav-bar.page.html',
   styleUrl: './nav-bar.page.css'
 })
 export class NavBarPage {
+  isNavOpen = false;
 
+  toggleNav() {
+    this.isNavOpen = !this.isNavOpen;
+  }
 }
