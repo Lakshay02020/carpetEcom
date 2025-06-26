1011e39c7f1b97645311705a69c65522ddd268a8
Lakshay Singla <lakshay02singla@gmail.com>
2025-06-26 17:28:46 +0530

    Changes

--- src/app/features/login/auth.page.ts
+++ src/app/features/login/auth.page.ts
@@ -5,13 +5,13 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth-service/auth.service';
 
 @Component({
-  selector: 'app-auth',
+  selector: 'app-login',
   standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.css']
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.css']
 })
-export class AuthPage {
+export class LoginPage {
   
   // Login form
   loginEmail = '';
@@ -42,23 +42,11 @@ export class AuthPage {
   }
 
   handleSignup() {
-    this.authService.signup({ 
-        name: this.signupName, 
-        email: this.signupEmail, 
-        password: this.signupPassword 
-    }).subscribe({
-    next: (res) => {
-      this.authService.saveToken(res.token);
-      this.router.navigate(['/']);
-    },
-    error: () => {
-      this.errorMessage = 'Signup failed. Please try again.';
-    }
-  });
+    // Replace with your actual signup logic
+    alert(`Signup with: ${this.signupName}, ${this.signupEmail}`);
   }
 
   togglePanel(showSignUp: boolean) {
     this.isRightPanelActive = showSignUp;
-    this.errorMessage = '';
   }
 }
