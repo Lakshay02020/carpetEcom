304b3a44db65e8ec3f2053f6712d7fe129bf1ac0
Lakshay Singla <lakshay02singla@gmail.com>
2025-06-28 11:01:30 +0530

    Merged small fixes and promotions added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    production: true,
-    productApi: 'https://product-service-i26l.onrender.com/api', 
-    cartApi: 'https://cart-service-kos9.onrender.com/api/cart', // Uncomment if you have a separate cart API
-    orderApi: 'https://order-service-7i5j.onrender.com/api/order', // Uncomment if you have a separate order API
-    authApi: 'https://user-service-vq73.onrender.com/api/auth'
+    production: false,
+    productApi: 'http://localhost:8090/api', // ✅ change this for dev
+    cartApi: 'http://localhost:8120/api/cart', // Uncomment if you have a separate cart API
+    orderApi: 'http://localhost:8011/api/order', // Uncomment if you have a separate order API
+    authApi : 'http://localhost:8888/api/auth'  
   };
